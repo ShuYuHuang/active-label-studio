ae804d31be04bf5a95b2e58ebeac1b88c875bd7e
Shu-Yu Huang <b123767195@gmail.com>
2023-01-08 21:46:47 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
+tar -xzf ngrok-v3-stable-linux-amd64.tgz
+sudo tar xvzf ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
+pip install label-studio
